fc168d344ba7362364e959d8a36e4700ad8543e6
fredsome <54943663+fredsome@users.noreply.github.com>
2020-06-23 20:01:28 -0500

    Ajout de premiere scene ,Setup

---
+++ PlayFab2/Assets/RememberRegister.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RememberRegister : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Toggle rememberToggle;
+    void Awake()
+    {
+        rememberToggle = GameObject.Find("Toggle").GetComponent<Toggle>();
+        if (rememberToggle.isOn) {
+            //On charge la valeur sovegarder au chargement
+            gameObject.GetComponent<InputField>().text = PlayerPrefs.GetString(gameObject.name);
+        }
+    }
+
+    // Update is called once per frame
+   public void SaveChange ()
+    {
+        if (rememberToggle.isOn) {
+            //On cree une clée
+            PlayerPrefs.SetString(gameObject.name,transform.Find("Text").GetComponent<Text>().text);
+        }
+    }
+}
---
+++ PlayFab2/Assets/RememberToggle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RememberToggle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (PlayerPrefs.GetInt("Toggle") > 0)
+        {
+
+            GetComponent<Toggle>().isOn = true;
+        }
+        else {
+
+            GetComponent<Toggle>().isOn = false;
+        }
+    }
+
+    // Update is called once per frame
+    public void SaveChange()
+    {
+        if (GetComponent<Toggle>().isOn)
+        {
+            PlayerPrefs.SetInt("Toggle", 1);
+
+
+        }
+        else { 
+        PlayerPrefs.SetInt("Toggle", 0);
+        }
+    }
+}
