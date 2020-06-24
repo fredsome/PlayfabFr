1a2d8f2ae7299e51c6c8173fb1d138c06f49c881
fredsome <54943663+fredsome@users.noreply.github.com>
2020-06-23 23:23:41 -0500

    cour playfab system de chargement cour 9

---
+++ PlayFab2/Assets/AuthScritph.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab.ClientModels;
+using UnityEngine.UI;
+using PlayFab.ClientModels;
+using PlayFab;
+using System;
+
+public class AuthScritph : MonoBehaviour
+{
+
+    [SerializeField]
+    // Start is called before the first frame update
+
+    InputField ifEmail, ifPassword, ifDisplayName;
+    public void RegisterPlayer()
+    {
+        var request = new RegisterPlayFabUserRequest() {
+            Email = ifEmail.text,
+            Password =ifPassword.text,
+            DisplayName =ifDisplayName.text,
+            Username = ifDisplayName.text
+       
+        };
+        PlayFab.PlayFabClientAPI.RegisterPlayFabUser(request,Success,Failed);
+
+        
+    }
+
+    private void Failed(PlayFabError err)
+    {
+        Debug.Log(err.ErrorMessage);
+    }
+
+    private void Success(RegisterPlayFabUserResult success)
+    {
+        Debug.Log("Enregistrement Reussit");
+        Debug.Log("ID :"+ success.PlayFabId);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
